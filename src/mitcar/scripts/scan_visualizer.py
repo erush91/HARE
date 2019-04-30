a12b78c7c7db0fbb1c92fa2bf1b7f274020e4140
mikeflanigan <flanman1866@gmail.com>
2019-04-29 18:44:12 -0600

    updated the scan visualizer to show a trace of the control magnitudes

--- src/mitcar/scripts/scan_visualizer.py
+++ src/mitcar/scripts/scan_visualizer.py
@@ -5,16 +5,22 @@ from math import sin, cos
 import rospy 
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float32MultiArray
+from plan_ctrl.msg import CarState
 import matplotlib.pyplot as plt
 
 # ~~ Setting and Constants ~~
 numReadings   = 100
 RunningOdroid =   1
 threshold     =   9.0
+preturn_thresh = 5.0
 plotCarts     =   1
 
 # ~~ Variables ~~
 lastScan = [ 0 for i in range( numReadings ) ]
+car_state = ''
+up = [0]*30
+ui = [0]*30
+ud = [0]*30
 
 def scan_cb( msg ):
     global lastScan
@@ -41,11 +47,23 @@ def cart_scan( arr ):
     arrLen = len( arr )
     angles = np.linspace( minAng , maxAng , arrLen )
     return [ polr_2_cart_0Y(  [ arr[i] , angles[i] ] ) for i in xrange( arrLen ) ]
+    
+def state_cb(state_rpt):
+	global car_state, ui, up, ud
+	up.pop(0)
+	ui.pop(0)
+	ud.pop(0)
+	up.append(state_rpt.up)
+	ui.append(state_rpt.ui)
+	ud.append(state_rpt.ud)
+	#print(round(state_rpt.up,3), round(state_rpt.ui,3), round(state_rpt.ud,3))
+	car_state = state_rpt.state
 	
 rospy.init_node( 'scan_sherlock' , anonymous = True )
 	
 if RunningOdroid: 
     rospy.Subscriber( "/filtered_distance" , Float32MultiArray , scan_cb )
+    rospy.Subscriber( "/ctrl_state_report" , CarState , state_cb )
 else: 
     rospy.Subscriber( "/scan" , LaserScan , scan_cb )
 	
@@ -53,8 +71,11 @@ try:
 	while ( not rospy.is_shutdown() ):
 		lastScanNP   = np.asarray( lastScan )
 		above_thresh = np.where( lastScanNP > threshold )[0]
-		# print(above_thresh)
-		# print(lastScan)
+
+		sorted_scan_inds = lastScanNP.argsort() # sorted from smallest to largest
+		N_largest_inds = sorted_scan_inds[-15:]
+		largest = lastScanNP[N_largest_inds]
+
 		plt.clf() # Clear all figures
 		
 		# Figure 1 , Highlighted Scan
@@ -63,12 +84,22 @@ try:
 		plt.plot( lastScanNP , 'bo' )
 		plt.ylim( [ 0 , 20 ] )
 		plt.hold( True )
-		vis_maxes = np.zeros( ( len( lastScan ) ) )
-		if len( above_thresh ) >=2:
-			vis_maxes[ above_thresh ] = lastScanNP[ above_thresh ]
-			plt.plot( vis_maxes , 'ro' )
-			# print(np.mean(above_thresh))
+		if len( above_thresh ) >=5:
+			plt.plot( above_thresh, lastScanNP[ above_thresh ] , 'ro' )
+			plt.plot(N_largest_inds, largest,'go')
+		else:
+			lastScanNP[-20:] = lastScanNP[-20:] + 2
+			above_thresh = np.where( lastScanNP > preturn_thresh )[0]
+			sorted_scan_inds = lastScanNP.argsort() # sorted from smallest to largest
+			N_largest_inds = sorted_scan_inds[-15:]
+			largest = lastScanNP[N_largest_inds]
+			if len( above_thresh ) >=5:
+				plt.plot( above_thresh, lastScanNP[ above_thresh ] , 'ro' )
+				plt.plot(N_largest_inds, largest,'go')
+		#elif len( above_thresh ) < 2 and np.std(lastScanNP) > 1:
+			#plt.plot(N_largest_inds, largest,'go')
 		plt.hold( False )
+		plt.title(car_state)
 		
 		if plotCarts:
 		    plt.figure(2)
@@ -77,6 +108,18 @@ try:
 		    Y = [ elem[1] for elem in points ]		    
 		    plt.scatter( X , Y )
 		    plt.axis( 'equal' )
+		    plt.title(car_state)
+		    
+		if plotCarts: plt.figure(3)
+		else: plt.figure(2)
+		plt.plot(up,'k')
+		plt.hold(True)
+		plt.plot(ui,'g')
+		plt.plot(ud,'r')
+		plt.ylim([-0.5, 0.5])
+		plt.xlim([0,25])
+		plt.legend(['P','I','D'])
+		plt.title('Control inputs')
 		
 		plt.pause( 0.001 )
 	plt.show()
