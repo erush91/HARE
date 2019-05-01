e230dabc18fe4e13b71247dc61c5c1d75334ee66
jwatson-CO-edu <james.watson-2@colorado.edu>
2019-05-01 13:27:32 -0600

    Error buf clear operation, needs testing

--- src/plan_ctrl/scripts/simple_PID_on_wall.py
+++ src/plan_ctrl/scripts/simple_PID_on_wall.py
@@ -46,6 +46,10 @@ from std_msgs.msg import Float32MultiArray
 from geometry_msgs.msg import PoseStamped, Point, Twist
 from ros_pololu_servo.msg import MotorCommand
 from ros_pololu_servo.msg import HARECommand
+import serial # pySerial for USB comms w/ Pololu
+from serial.tools import list_ports
+import struct # for data composition over serial
+
 # ~ Custom Messages ~
 from plan_ctrl.msg import CarState
 from rosflight_msgs.msg import RCRaw
@@ -129,6 +133,60 @@ def max_dex( numLst ):
     """ Return the maximum index , First incidence """
     # NOTE: This function assumes that 'numLst' has only numeric elements
     return numLst.index( max( numLst ) )
+
+def attempt_connection( COMlist , speed ):
+    """ Attempt to connect to each of 'COMlist' until one succeeds """
+    ser = None
+    for port in COMlist:
+        try:
+            ser = serial.Serial( port , speed ) # This must match the port selected in the Arduino IDE
+            print "Connected to port:" , ser.name 
+            return ser
+        except Exception as ex:
+            print "ERROR: COULD NOT ESTABLISH SERIAL CONNECTION WITH" , port , ", Check that the port is correct ..."
+            print ex
+    raise Warning( "attempt_connection: Unable to connect to any of " + str( COMlist ) ) # If we made it to here , we attempted all the ports
+
+def list_available_ports():
+    """ List all the ports that pyserial can detect , Results are dependent on operating system """
+    ports = list_ports.comports()
+    for port in ports:
+        print port.device , '\t' , port.manufacturer , '\t' , port.description
+    return [ port.device for port in ports  ]
+
+def send_pololu_error_clear( connection ):
+    """ Spam the Maestro with error buffer requests """
+    # https://www.pololu.com/docs/0J40/5.e
+    # Compact Protocol: This is the simpler and more compact of the two protocols; 
+    #                   it is the protocol you should use if your Maestro is the only device connected to your serial line.
+    compact = [ 162 ] # ----------- Compact protocol: 0xA2 # int('a2',16) = 162
+    polProt = [ 170 ,   0 ,  34 ] # Pololu protocol: 0xAA, device number, 0x22    
+    msg     = bytearray( compact )
+    repeat  = 5
+    for i in xrange( repeat ):
+        connection.write( msg )
+        rospy.sleep( 0.001 )
+    print "Spammed serial connection with byte message" , msg , "," , repeat , "times!"
+        
+def clear_pololu_err_buf():
+    """ Connect w/ Maestro, Spam error clear, Close connection """
+    COMLIST = [ '/dev/pololu' ]
+    COMBAUD = 115200
+    pol_ser = attempt_connection( COMLIST , COMBAUD )
+    if pol_ser:
+        print "Connection open!"
+    try:
+        send_pololu_error_clear( pol_ser )
+        if pol_ser.in_waiting:
+            readResult = pol_ser.read( pol_ser.in_waiting )
+            print "Error Bytes:" , readResult                 
+        else:
+            print "There were no error bytes to read ..."
+        pol_ser.close()
+        print "Connection closed!"
+    except Exception as ex:
+        print "Pololu error clear operation failed for the following reason:"
+        print ex                
 
 # __ End Func __
 
@@ -235,6 +293,8 @@ class CarFSM:
         rospy.Subscriber( "/rc_raw", RCRaw, self.rc_cb )
         self.rc_msg = RCRaw()
         
+        # 10. Attempt to clear error buffer
+        clear_pololu_err_buf()
 
     def scan_cb( self , msg ):
         """ Process the scan that comes back from the scanner """
