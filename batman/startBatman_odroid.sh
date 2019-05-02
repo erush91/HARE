63f65ef6ed7cdc3bea61885b27fe439d5ac4079d
robot <robot@robot.com>
2019-05-01 19:30:37 -0600

    Fixed batman dir bug

--- batman/startBatman_odroid.sh
+++ batman/startBatman_odroid.sh
@@ -1,4 +1,4 @@
 sudo modprobe batman-adv
 sudo systemctl stop NetworkManager
-sudo ${PWD}/HARE/batman/setupBatman_odroid.sh
-sudo ${PWD}/HARE/batman/setupBatman_odroid.sh
+sudo /home/odroid/HARE/batman/setupBatman_odroid.sh
+sudo /home/odroid/HARE/batman/setupBatman_odroid.sh
